51cb84468ba138de751352595fd12dd2f2643219
Deepak <deepakappu961@gmail.com>
2025-06-24 19:10:40 +0530

    feat: add morgan for logging and protect movie routes with authentication

--- routes/Movie.js
+++ routes/Movie.js
@@ -2,13 +2,14 @@ const express = require('express')
 
 const Movie = require('../models/Movie');
 const { getAllMovies, getMovieById, createMovie, updateMovie, deleteMovie } = require('../controller/MovieController');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/movies', getAllMovies);
 router.get('/getMovieById', getMovieById);
-router.post('/createMovie', createMovie);
-router.put('/updateMovie/:id', updateMovie);
-router.delete('/deleteMovie/:id', deleteMovie);
+router.post('/createMovie', protect, createMovie);
+router.put('/updateMovie/:id', protect, updateMovie);
+router.delete('/deleteMovie/:id', protect, deleteMovie);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const morgan = require('morgan')
 
 const app = express();
 app.use(express.json());
+app.use(morgan(`:remote-addr [:date] ":method :url" :status - :response-time ms`));
+
+morgan.token('date', () => {
+  const date = new Date();
+
+  const formatted = date.toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
+
+  const capitalized = formatted.replace(/(am|pm)/, (match) => match.toUpperCase());
+  return `${capitalized} IST`;
+});
 
 const { swaggerUi, swaggerSpec } = require('./swagger');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
